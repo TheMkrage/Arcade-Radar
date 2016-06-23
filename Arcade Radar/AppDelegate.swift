150be16d3a42cd9ae3fe3b39aad3564c72b0dc5d
TheMkrage <metoofarmer@yahoo.com>
2016-06-22 23:53:06 -0700

    a

--- Arcade Radar/AppDelegate.swift
+++ Arcade Radar/AppDelegate.swift
@@ -12,10 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    let APP_ID = "FFA992AA-0B7F-8776-FFB4-92B410F98800"
-    let SECRET_KEY = "958B8D5E-0D85-30C8-FF63-A21D5698FF00"
-    let VERSION_NUM = "v1"
-    
+       
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         let backendless = Backendless.sharedInstance()
         backendless.initApp(APP_ID, secret:SECRET_KEY, version:VERSION_NUM)
